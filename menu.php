b191a6f2039ab90f28e34e76aa4f536a87547bc7
DylCraXs <132933589+DylCraXs@users.noreply.github.com>
2023-09-04 17:36:41 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -4,20 +4,20 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Menu general</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="css/menu.css">
 </head>
 <body>
 	<div class="header">
-	<h3 class="titulo">Menu General</h3>
-	
-	<div class="menu">		
-	
-	<a href="form-altas.php">Alta</a><br>
-	<a href="form-bajas.php">Bajas</a><br>
-	<a href="form-modificacion.php">Modificaciones</a><br>
-	<a href="listar.php">Listado de personas</a><br>
-
-	</div>
+		<h3 class="titulo">Menu General</h3>
+		<div class="menu">		
+			<a href="form-altas.php">Alta</a><br>
+			<a href="form-bajas.php">Bajas</a><br>
+			<a href="form-modificacion.php">Modificaciones</a><br>
+			<a href="listar.php">Listado de personas</a><br>
+		</div>
 	</div>
 	
 	
